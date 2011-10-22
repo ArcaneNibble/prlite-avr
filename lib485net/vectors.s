e41cf67055bbf5655be031f932c8e51a6b3ce536
Robert Ou <someh4x0r@gmail.com>
2011-10-22 14:39:47 -0700

    vectors for stream mode

--- lib485net/vectors.s
+++ lib485net/vectors.s
@@ -10,16 +10,28 @@ ret
 _jumptable:
 	rjmp initLib
 	rjmp getVersion
+
 	rjmp uart_rx_isr
 	rjmp uart_tx_isr
+
 	rjmp t2_150
 	rjmp t2_300
+
 	rjmp setAddr
+
 	rjmp sendRaw
 	rjmp recvRaw
 	rjmp peekPackets
+
 	rjmp doChecksum
+
 	rjmp connectDGram
 	rjmp closeDGram
 	rjmp sendDGram
 	rjmp recvDGram
+
+	rjmp listenStream
+	rjmp connectStream
+	rjmp sendStream
+	rjmp recvStream
+	rjmp closeStream
