1ce826ccb7228bb7c3e2f6a7ea4cdfdedd7b866b
Robert Ou <someh4x0r@gmail.com>
2011-10-29 18:17:06 -0700

    interface done

---
+++ includes/lib485net.h
@@ -0,0 +1,17 @@
+extern void getVersion(char *v);
+extern void initLib(void);
+extern void setAddr(unsigned char a);
+extern unsigned char doChecksum(const unsigned char *buf, unsigned char len);
+extern unsigned char sendRaw(const unsigned char *packet, unsigned char len);
+extern unsigned char recvRaw(unsigned char *packet, unsigned char *len);
+extern unsigned char peekPackets(void);
+extern void *connectDGram(unsigned char addr, unsigned char localport, unsigned char remoteport);
+extern void *listenDGram(unsigned char localport);
+extern unsigned char sendDGram(void *conn, const unsigned char *packet, unsigned char len);
+extern unsigned char recvDGram(void *conn, unsigned char *packet, unsigned char *len);
+extern void closeDGram(void *conn);
+extern void *listenStream(unsigned char localport);
+extern void *connectStream(unsigned char addr, unsigned char localport, unsigned char remoteport);
+extern unsigned char sendStream(void *conn, const unsigned char *packet, unsigned char len);
+extern unsigned char recvStream(void *conn, unsigned char *packet, unsigned char *len);
+extern void closeStream(void *conn);
