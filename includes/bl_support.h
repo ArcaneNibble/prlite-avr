706d8807ceed4d536d925a8d7693e163a7489e06
Robert Ou <someh4x0r@gmail.com>
2011-11-12 15:15:55 -0800

    add erase csum (to reload) functions

--- includes/bl_support.h
+++ includes/bl_support.h
@@ -3,5 +3,7 @@
 
 extern void bl_reboot(void);
 extern unsigned char bl_get_addr(void);
+extern void bl_erase_lib_csum(void);
+extern void bl_erase_app_csum(void);
 
 #endif
--- libs/bl_support/bl_support.c
+++ libs/bl_support/bl_support.c
@@ -4,6 +4,15 @@
 
 #include "bl_support.h"
 
+typedef struct
+{
+	unsigned char address;
+	unsigned long library_adler;
+	unsigned long application_adler;
+	unsigned char data[6];
+	unsigned char checksum;
+} __attribute__((__packed__)) bootdata;
+
 void bl_reboot(void)
 {
 	wdt_enable(WDTO_15MS);
@@ -15,4 +24,40 @@ unsigned char bl_get_addr(void)
 {
 	//checksum should be checked by bootloader
 	return eeprom_read_byte((void*)(0x3F0));
+}
+
+void bl_erase_lib_csum(void)
+{
+	bootdata d;
+	unsigned char i, a;
+	unsigned char *dp;
+	
+	eeprom_read_block(&d, (void*)(0x3F0), sizeof(d));
+	d.library_adler = 0xffffffff;
+	
+	dp = (unsigned char *)(&d);
+	a = 0;
+	for(i=0;i<sizeof(d)-1;i++)
+		a += dp[i];
+	d.checksum = a;
+		
+	eeprom_write_block(&d, (void*)(0x3F0), sizeof(d));
+}
+
+void bl_erase_app_csum(void)
+{
+	bootdata d;
+	unsigned char i, a;
+	unsigned char *dp;
+	
+	eeprom_read_block(&d, (void*)(0x3F0), sizeof(d));
+	d.application_adler = 0xffffffff;
+	
+	dp = (unsigned char *)(&d);
+	a = 0;
+	for(i=0;i<sizeof(d)-1;i++)
+		a += dp[i];
+	d.checksum = a;
+		
+	eeprom_write_block(&d, (void*)(0x3F0), sizeof(d));
 }
